013a021916a9d641a48c9d4f823786b034dda233
ivy <aibiimaknae@gmail.com>
2022-12-18 15:03:47 -0600

    finished home screen

--- diary/src/pages/Home.jsx
+++ diary/src/pages/Home.jsx
@@ -1,14 +1,40 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
 
   const posts =[
-    
+    {
+      id: 1,
+      title: "lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 2,
+      title: "lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    },
+    {
+      id: 3,
+      title: "lorem ipsum",
+      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    }
   ]
 
   return (
     <div className='home'>
-      Home
+      <div className='posts'>
+        {posts.map(post=>(
+          <div className='post' key={post.id}>
+            <div className="content">
+              <Link className='link' to={'/post/${post.id}'}>
+              <h1>{post.title}</h1>
+              </Link>
+              <p>{post.desc}</p>
+            </div>
+          </div>
+        ))}
+      </div>
       </div>
   )
 }
