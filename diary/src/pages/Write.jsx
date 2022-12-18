ad4b19f27b3c5cbc32f6a591d3173be6356d318d
ivy <aibiimaknae@gmail.com>
2022-12-18 16:22:26 -0600

    finished styling write post page time for a break bc im braindead

--- diary/src/pages/Write.jsx
+++ diary/src/pages/Write.jsx
@@ -10,12 +10,12 @@ const Write = () => {
       <div className="content">
         <input type="text" placeholder='title' />
         <div className="editorContainer">
-        <ReactQuill theme="snow" value={value} onChange={setValue} />
+        <ReactQuill className='editor' theme="snow" value={value} onChange={setValue} />
         </div>
       </div>
       <div className="menu">
         <div className="item">
-          <span><b>Status:</b> <i>Draft</i></span>
+          <h2><b>Status:</b> <i>Draft</i></h2>
           <div className="buttons">
           <button>Publish</button>
           <button>Save as a draft</button>
