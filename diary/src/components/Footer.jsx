2028036bda1b60e104d52ba00328d52a82970b47
ivy <aibiimaknae@gmail.com>
2022-12-18 13:29:22 -0600

    started footer, no styling

--- diary/src/components/Footer.jsx
+++ diary/src/components/Footer.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <span>made with 🩸, 💦, 😭, and <b>React.js</b></span><br></br>
+      <span><a target="_blank" href="https://icons8.com/icon/tHBlsmJhlmYA/diary">Diary</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/2QNRrIsXL2Si/delete-key">Delete Key</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>
+<a target="_blank" href="https://icons8.com/icon/hpGBYYSBgFIB/pencil">Pencil</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></span>
+    </footer>
   )
 }
 
